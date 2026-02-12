36933b2e5f6498a3922e581c708f43b94c3333cd
Tanishk Sancheti <tanishksancheti1@gmail.com>
2026-02-13 00:35:03 +0530

    fixed

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -12,7 +12,7 @@ export default function ProjectPage() {
 
   const fetchProjects = async () => {
     try {
-      const res = await axios.get("/projects");
+      const res = await axios.get("/api/projects");
       setProjects(res.data);
       setError("");
     } catch (err) {
